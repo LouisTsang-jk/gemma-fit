cc486ea17300fcac9e8ccd7b4a9b960550f5040f
LouisTsang <646168919@qq.com>
2024-11-23 12:16:01 +0800

    chore: remove turbopack

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="w-[320px]">
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input type="email" label="Email" />
+        <Input type="email" label="Email" placeholder="Enter your email" />
+      </div>
     </div>
   );
 }
