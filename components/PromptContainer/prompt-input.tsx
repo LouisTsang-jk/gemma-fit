349ec9d528dded8b86e5920af062784195a740a7
scarqin <1054139596@qq.com>
2024-11-23 16:56:08 +0800

    feat: change

--- components/PromptContainer/prompt-input.tsx
+++ components/PromptContainer/prompt-input.tsx
@@ -19,7 +19,7 @@ const PromptInput = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
           input: cn("py-0", classNames?.input),
         }}
         minRows={1}
-        placeholder="Enter a prompt here"
+        placeholder="有哪些提高睡眠质量的建议？"
         radius="lg"
         variant="bordered"
         {...props}
